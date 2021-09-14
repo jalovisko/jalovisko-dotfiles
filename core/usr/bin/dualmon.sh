32b85c219d26b3047e6df4d4bad3567bd349e6de
jalovisko <letovnn@gmail.com>
2021-09-14 17:36:49 -0400

    restructure of wallpapers and dualmon setup

--- core/usr/bin/dualmon.sh
+++ core/usr/bin/dualmon.sh
@@ -1,1 +1,3 @@
-xrandr --output ePD-1-0 --primary
+#!/bin/sh
+xrandr --output DP-1 --off --output HDMI-1 --mode 1920x1080 --rate 60.00 --pos 1920x0 --rotate normal --output DP-2 --mode 1920x1080 --rate 185.00 --pos 0x0 --rotate normal --output DP-3 --off
+feh --randomize --bg-scale /home/jalovisko/.config/Wallpapers/
